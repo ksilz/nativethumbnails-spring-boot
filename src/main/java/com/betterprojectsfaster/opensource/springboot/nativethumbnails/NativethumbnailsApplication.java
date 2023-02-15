e421e743d8db7755a535eed5d6c4083fd2626437
Karsten Silz <karsten.silz@outlook.com>
2023-02-15 14:10:19 +0000

    Initial version.

---
+++ src/main/java/com/betterprojectsfaster/opensource/springboot/nativethumbnails/NativethumbnailsApplication.java
@@ -0,0 +1,13 @@
+package com.betterprojectsfaster.opensource.springboot.nativethumbnails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativethumbnailsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NativethumbnailsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/betterprojectsfaster/opensource/springboot/nativethumbnails/NativethumbnailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterprojectsfaster.opensource.springboot.nativethumbnails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativethumbnailsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
